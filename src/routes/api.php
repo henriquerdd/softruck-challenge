7be608a0e6e67959821f6e172fbb66d3a0fb9ba3
Henrique <henriquerdd.10@gmail.com>
2019-07-21 11:37:42 -0300

    Definindo as rotas que nossa api vai disponibilizar

--- src/routes/api.php
+++ src/routes/api.php
@@ -13,6 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/tasks', 'TasksAPIController@all')->name('api.tasks.all');
+Route::post('/tasks', 'TasksAPIController@store')->name('api.tasks.store');
+
+Route::get('/tasks/{uuid}', 'TasksAPIController@find')->name('api.tasks.find');
+Route::patch('/tasks/{uuid}', 'TasksAPIController@update')->name('api.tasks.update');
+Route::delete('/tasks/{uuid}', 'TasksAPIController@destroy')->name('api.tasks.destroy');
+
+Route::get('/boards', 'BoardsAPIController@all')->name('api.boards.all');
+Route::post('/boards', 'BoardsAPIController@store')->name('api.boards.store');
+
+Route::get('/boards/{uuid}', 'BoardsAPIController@find')->name('api.boards.find');
+Route::patch('/boards/{uuid}', 'BoardsAPIController@update')->name('api.boards.update');
+Route::delete('/boards/{uuid}', 'BoardsAPIController@destroy')->name('api.boards.destroy');
+
+Route::get('/boards/{uuid}/tasks', 'BoardsAPIController@tasks')->name('api.boards.tasks');
