54d536c633a8e0983d6027040907f0de97268628
Henrique <henriquerdd.10@gmail.com>
2019-07-24 17:10:13 -0300

    Removi tudo que tinha adicionado em app.js, mas por via das dúvidas vou coloca-lo no final da view principal, pois se acabar adicionando algo nele, e isso tiver dependências ela certamente já terá sido inclusa

--- src/resources/views/vendor/adminlte/master.blade.php
+++ src/resources/views/vendor/adminlte/master.blade.php
@@ -49,8 +49,6 @@
 <script src="{{ asset('vendor/adminlte/vendor/jquery/dist/jquery.slimscroll.min.js') }}"></script>
 <script src="{{ asset('vendor/adminlte/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
-<script src="{{ asset('js/app.js') }}"></script>
-
 @if(config('adminlte.plugins.select2'))
     <!-- Select2 -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
@@ -67,6 +65,8 @@
 @endif
 
 @yield('adminlte_js')
+
+<script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
