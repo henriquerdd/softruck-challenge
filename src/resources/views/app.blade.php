4d7d5a8116a1d47dd5b8ea8beb7baa37a103ddcc
Henrique <henriquerdd.10@gmail.com>
2019-07-25 19:27:33 -0300

    Essa será a unica view da nossa aplicação

---
+++ src/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+{{-- resources/views/admin/dashboard.blade.php --}}
+
+@extends('adminlte::page')
+
+@section('title', 'Tasks Manager')
+
+@section('content_header')
+    
+@stop
+
+@section('content')
+    <div id='app'></div>
+@stop
