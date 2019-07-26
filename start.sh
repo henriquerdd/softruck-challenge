0343737cfb69906eb30a67901bd59aa5db5915c8
Henrique <henriquerdd.10@gmail.com>
2019-07-26 12:27:55 -0300

    Atualizando comandos

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-docker-compose up -d svc-nginx svc-php-fpm svc-mysql
+docker-compose up -d svc-nginx svc-php-fpm svc-mysql && 
+docker-compose run -u $UID --entrypoint "php artisan config:cache" svc-php-fpm &&
+docker-compose run -u $UID --entrypoint "php artisan migrate" svc-php-fpm
