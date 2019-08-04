118834bb651406812e1a953f3ec5d45185d73ab4
Henrique <henriquerdd.10@gmail.com>
2019-08-04 19:13:33 -0300

    Agora temos menos telas

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,41 +1,38 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import ApiClient from './apiClient'
-import TasksList from './components/tasks/tasksList';
-import TasksCreator from './components/tasks/tasksCreate';
-import TasksChanger from './components/tasks/tasksUpdate';
-import BoardsList from './components/boards/boardsList';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import Header from './components/app/header';
+import SideMenu from './components/app/sidemenu';
+import ApiClient from './apiClient';
+import Board from './components/boards/board';
 import BoardsCreator from './components/boards/boardsCreate';
 import BoardsChanger from './components/boards/boardsUpdate';
+import TasksChanger from './components/tasks/tasksUpdate';
 
-const apiClient = new ApiClient();
+var apiClient = new ApiClient();
 
 class App extends Component {
-  render () {
-    return (
-      <BrowserRouter>
-        <div>
-            <Switch>
-                <Route exact path="/tasks" render={(props) => <TasksList {...props} apiClient={apiClient} />} />
-                <Route exact path="/tasks/create" render={(props) => <TasksCreator {...props} apiClient={apiClient} />} />
-                <Route exact path="/tasks/:taskUuid" render={(props) => <TasksChanger {...props} apiClient={apiClient} />} />
-                <Route exact path="/boards" render={(props) => <BoardsList {...props} apiClient={apiClient} />} />
-                <Route exact path="/boards/create" render={(props) => <BoardsCreator {...props} apiClient={apiClient} /> } />
-                <Route exact path="/boards/:boardUuid" render={(props) => <BoardsChanger {...props} apiClient={apiClient} /> } />
-            </Switch>
-        </div>
-      </BrowserRouter>
-    )
-  }
+  
+    render () {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Header title="Quadros" />
+                    <SideMenu apiClient={apiClient} />
+                    <div class="content-wrapper">
+                        <Switch>
+                            <Route exact path="/boards/:boardUuid" render={(props) => <Board {...props} apiClient={apiClient} />} />
+                            <Route exact path="/create/boards" render={(props) => <BoardsCreator {...props} apiClient={apiClient} /> } />
+                            <Route exact path="/boards/:boardUuid/edit" render={(props) => <BoardsChanger {...props} apiClient={apiClient} /> } />
+                            <Route exact path="/tasks/:taskUuid/edit" render={(props) => <TasksChanger {...props} apiClient={apiClient} />} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
